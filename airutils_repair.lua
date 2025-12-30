c72a85109b8e8c93d4eaffba99f7c72d4ffd9813
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-12-30 10:39:20 -0300

    Fix formatting issues in airutils_repair.lua tabs to spaces

--- airutils_repair.lua
+++ airutils_repair.lua
@@ -2,15 +2,15 @@ local S = airutils.S
 
 -- trike repair
 core.register_craftitem("airutils:repair_tool",{
-	description = S("Repair Tool"),
-	inventory_image = "airutils_repair_tool.png",
+    description = S("Repair Tool"),
+    inventory_image = "airutils_repair_tool.png",
 })
 
 core.register_craft({
     output = "airutils:repair_tool",
     recipe = {
-	    {"", "default:steel_ingot", ""},
-	    {"", "default:steel_ingot", ""},
-	    {"default:steel_ingot", "", "default:steel_ingot"},
+        {"", "default:steel_ingot", ""},
+        {"", "default:steel_ingot", ""},
+        {"default:steel_ingot", "", "default:steel_ingot"},
     },
 })
